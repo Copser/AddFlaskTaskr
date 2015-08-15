88678283cbe6a5e4fb879512a922ed570796e818
Copser <iamrmot@gmail.com>
2015-08-15 21:32:33 +0200

    Creating new db_create.py for creating new flasktaskr.db whit SQLAlchemy.

--- project/db_create.py
+++ project/db_create.py
@@ -1,25 +1,18 @@
 # project/db_create.py
+from views import db
+from models import Task
+from datetime import date
 
+# create the databse and the db table
+db.create_all()
 
-import sqlite3
-from _config import DATABASE_PATH
+# insert data
+db.session.add(Task("Finish this tutorial", date(2015, 20, 9), 10, 1))
+db.session.add(Task("Finish Real Python", date(2015, 20, 12), 10, 1))
+db.session.add(Task("Database Menagement", date(2015, 15, 9), 1, 1))
+db.session.add(Task("User Registration", date(2015, 16, 9), 2, 1))
+db.session.add(Task("User Login/Authentication", date(2015, 17, 9), 3, 1))
+db.session.add(Task("Database Relationships", date(2015, 18, 9), 4, 1))
 
-with sqlite3.connect(DATABASE_PATH) as connection:
-
-    # get a cursor object used to execute SQL commands
-    c = connection.cursor()
-
-    # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL,
-        status INTEGER NOT NULL)""")
-
-    # insert dummy data into the table
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish this tutorial", "03/25/2015", 10, 1)'
-    )
-    c.execute(
-        'INSERT INTO tasks (name, due_date, priority, status)'
-        'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)'
-    )
+# commit the changes
+db.session.commit()

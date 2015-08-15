fed52ce4105d28237f1c772b820937a916732781
Copser <iamrmot@gmail.com>
2015-08-15 21:41:04 +0200

    Refactoring db_create, cleaning Value Error.

--- project/db_create.py
+++ project/db_create.py
@@ -7,12 +7,12 @@ from datetime import date
 db.create_all()
 
 # insert data
-db.session.add(Task("Finish this tutorial", date(2015, 20, 9), 10, 1))
-db.session.add(Task("Finish Real Python", date(2015, 20, 12), 10, 1))
-db.session.add(Task("Database Menagement", date(2015, 15, 9), 1, 1))
-db.session.add(Task("User Registration", date(2015, 16, 9), 2, 1))
-db.session.add(Task("User Login/Authentication", date(2015, 17, 9), 3, 1))
-db.session.add(Task("Database Relationships", date(2015, 18, 9), 4, 1))
+db.session.add(Task("Finish this tutorial", date(2015, 9, 30), 10, 1))
+db.session.add(Task("Finish Real Python", date(2015, 10, 20), 10, 1))
+db.session.add(Task("Database Menagement", date(2015, 9, 15), 1, 1))
+db.session.add(Task("User Registration", date(2015, 9, 16), 2, 1))
+db.session.add(Task("User Login/Authentication", date(2015, 9, 17), 3, 1))
+db.session.add(Task("Database Relationships", date(2015, 9, 18), 4, 1))
 
 # commit the changes
 db.session.commit()

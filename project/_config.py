16e6c1f63b9e32db1086df842afdb58d25dfafc1
Copser <iamrmot@gmail.com>
2015-08-15 07:27:57 +0200

    Adding proper secret_key.

--- project/_config.py
+++ project/_config.py
@@ -10,7 +10,7 @@ DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'my_precious'
+SECRET_KEY = 'E\xf37z*\xd0\x96\xea}[\xea\xe2\xcad;\xd2_\xd3\x02\xc1\x98M\xf5'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)

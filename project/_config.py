544126f9abbdf7b6a4ec124d786c2fded060affb
Copser <iamrmot@gmail.com>
2015-08-16 05:45:57 +0200

    Deleting raw data because we are going to use information from the new users table

--- project/_config.py
+++ project/_config.py
@@ -7,8 +7,8 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-USERNAME = 'admin'
-PASSWORD = 'admin'
+# USERNAME = 'admin'
+# PASSWORD = 'admin'
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'E\xf37z*\xd0\x96\xea}[\xea\xe2\xcad;\xd2_\xd3\x02\xc1\x98M\xf5'
 

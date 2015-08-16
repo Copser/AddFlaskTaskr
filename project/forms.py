1ffd22fd90a6c5697bb56ea90692317b29467b48
Copser <iamrmot@gmail.com>
2015-08-16 07:22:01 +0200

    Fixin Typos.

--- project/forms.py
+++ project/forms.py
@@ -37,12 +37,10 @@ class RegisterForm(Form):
     )
     password = PasswordField(
         'Password',
-        validators=[DataRequired(), Length(min=6, max=40)]
-    )
+        validators=[DataRequired(), Length(min=6, max=40)])
     confirm = PasswordField(
         'Repeat Password',
-        validators=[DataRequired(), EqualTo('password',
-                    message='Password must match!')]
+        validators=[DataRequired(), EqualTo('password', message='Password must match!')]
     )
 
 

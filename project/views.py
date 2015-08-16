e3d58b4c62514b4d44c629c5f3d407458740f6bf
Copser <iamrmot@gmail.com>
2015-08-16 09:58:03 +0200

    Refactoring tasks.html and adding tasks to are new_task function controller.

--- project/views.py
+++ project/views.py
@@ -1,4 +1,5 @@
 # project/views.py
+import datetime
 from functools import wraps
 
 from forms import AddTaskForm, RegisterForm, LoginForm
@@ -99,6 +100,8 @@ def new_task():
                 form.name.data,
                 form.due_date.data,
                 form.priority.data,
+                datetime.datetime.utcnow(),
+                '1',
                 '1'
             )
             db.session.add(new_task)

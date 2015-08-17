6f0e13b815481b216d6c5575b33f85c8bebd7cf5
Copser <iamrmot@gmail.com>
2015-08-17 06:28:07 +0200

    Password can now handle 4 digit password!

--- project/forms.py
+++ project/forms.py
@@ -37,7 +37,7 @@ class RegisterForm(Form):
     )
     password = PasswordField(
         'Password',
-        validators=[DataRequired(), Length(min=6, max=40)])
+        validators=[DataRequired(), Length(min=4, max=40)])
     confirm = PasswordField(
         'Repeat Password',
         validators=[DataRequired(), EqualTo('password', message='Password must match!')]

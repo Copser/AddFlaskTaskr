ebba2530d63af68bb42ed6f7f7ee650672e07b80
Copser <iamrmot@gmail.com>
2015-08-22 06:16:33 +0200

    Refactoring afther debuging tests.

--- project/_config.py
+++ project/_config.py
@@ -3,17 +3,16 @@
 
 import os
 
+
 # grab the folder where this script lives
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DATABASE = 'flasktaskr.db'
-# USERNAME = 'admin'
-# PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'E\xf37z*\xd0\x96\xea}[\xea\xe2\xcad;\xd2_\xd3\x02\xc1\x98M\xf5'
+CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
 
 # the database uri
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
--- project/db_create.py
+++ project/db_create.py
@@ -1,18 +1,17 @@
 # project/db_create.py
+
+
 from views import db
 from models import Task
 from datetime import date
 
-# create the databse and the db table
+
+# create the database and the db table
 db.create_all()
 
 # insert data
-# db.session.add(Task("Finish this tutorial", date(2015, 9, 30), 10, 1))
-# db.session.add(Task("Finish Real Python", date(2015, 10, 20), 10, 1))
-# db.session.add(Task("Database Menagement", date(2015, 9, 15), 1, 1))
-# db.session.add(Task("User Registration", date(2015, 9, 16), 2, 1))
-# db.session.add(Task("User Login/Authentication", date(2015, 9, 17), 3, 1))
-# db.session.add(Task("Database Relationships", date(2015, 9, 18), 4, 1))
+# db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
+# db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, 1))
 
 # commit the changes
 db.session.commit()

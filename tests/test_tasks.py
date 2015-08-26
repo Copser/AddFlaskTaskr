cca8b77d2d344628439656a1d32a202483597554
Copser <iamrmot@gmail.com>
2015-08-26 21:38:04 +0200

    Buidling tests for users.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -214,5 +214,63 @@ class TasksTests(unittest.TestCase):
         response = self.app.get('tasks/', follow_redirects=True)
         self.assertIn(b'Fletcher', response.data)
 
+
+    def test_users_cannot_see_modify_links_for_tasks_not_created_by_them(self):
+        """TODO: Docstring for test_users_cannot_see_modify_links_for_tasks_not_created_by_them.
+        :returns: TODO
+
+        """
+        self.register('Michael', 'michael@realpython.com',
+                'python', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.register('Fletcher', 'fletcher@realpython.com', 'python101',
+                'python101')
+        response = self.login('Fletcher', 'python101')
+        self.app.get('tasks/', follow_redirects=True)
+        self.assertNotIn(b'Mark as complete', response.data)
+        self.assertNotIn(b'Delete', response.data)
+
+    def test_users_can_see_task_modify_links_for_tasks_created_by_them(self):
+        """TODO: Docstring for test_users_can_see_task_modify_links_for_tasks_created_by_them.
+        :returns: TODO
+
+        """
+        self.register('Michael', 'michael@realpython.com',
+                'python', 'python')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.register('Fletcher', 'fletcher@realpython.com',
+                'python101', 'python101')
+        self.login('FLetcher', 'python101')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.create_task()
+        self.assertIn(b'complete/2/', response.data)
+        self.assertIn(b'complete/2/', response.data)
+
+    def test_admin_users_can_see_task_modify_links_for_all_tasks(self):
+        """TODO: Docstring for test_admin_users_can_see_task_modify_links_for_all_tasks.
+        :returns: TODO
+
+        """
+        self.register('Michael', 'michael@realpython.com',
+                'python', 'pyhon')
+        self.login('Michael', 'python')
+        self.app.get('tasks/', follow_redirects=True)
+        self.create_task()
+        self.logout()
+        self.create_admin_user()
+        self.login('Superman', 'allpowerful')
+        self.app.get('tasks/', follow_redirects=True)
+        response = self.create_task()
+        self.assertIn(b'complete/1/', response.data)
+        self.assertIn(b'delete/1/', response.data)
+        self.assertIn(b'complete/2/', response.data)
+        self.assertIn(b'delete/2/', response.data)
+
 if __name__ == "__main__":
     unittest.main()
